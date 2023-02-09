9b2b5d0357912a9a1fb30c6cf39c7f6988d9f16c
Phatcharaphan Ananpreechakun <phat.pan@gmail.com>
2023-02-09 17:44:49 +0700

    Add link source code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import { NextPage } from 'next';
 import {
     Container,
     Heading,
+    Text,
+    Link
 } from '@chakra-ui/react';
-import Part1 from '../components/Part1';
 import { useState } from 'react';
-import { calculator, findLastNumber, plotGraph } from '../utils/tasksUtils'
+import { calculator, findLastNumber, plotGraph } from '../utils/tasksUtils';
+import Part1 from '../components/Part1';
 import Part2 from '../components/Part2';
 import Part3 from '../components/Part3';
 
@@ -32,6 +34,7 @@ const IndexPage: NextPage = () => {
 
     return (<Container>
         <Heading mt={5} mb={5} as="h1">Tasks</Heading>
+        <Text color="gray.600" mt={5} mb={5}>Source code: <Link>https://github.com/phatpan/application-test</Link> </Text>
         <Part1 onClick={calculate} result={resultCalculate} />
         <Part2 onClick={calculateNumbers} result={numbers} />
         <Part3 onClick={calculateChart} data={dataChart} />
